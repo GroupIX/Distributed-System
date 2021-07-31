74a49d2319a7ef0dd04437e6103c83eceec6207b
GroupIX <88250926+GroupIX@users.noreply.github.com>
2021-07-31 22:39:41 +0300

    Current distributed system code. It has errors.

---
+++ PTSLibrary/PTSLibrary/DAO/CustomerDAO.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PTSLibrary.DAO
+{
+    class CustomerDAO : SuperDAO
+    {
+        public int Authenticate(string username, string password)
+        {
+            string sql;
+            SqlConnection cn;
+            SqlCommand cmd;
+            SqlDataReader dr;
+
+            sql = String.Format("SELECT CuatsomerId FROM Customer WHERE Username=(0) AND Password='(1)'", username, password);
+            cn = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            cmd = new SqlCommand(sql, cn);
+            int id = 0;
+            try
+            {
+                cn.Open();
+                dr = cmd.ExecuteReader(CommandBehavior.SingleRow);
+                if (dr.Read())
+                {
+                    id = (int)dr["CustomerId"];
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Accessing Database", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return id;
+        }
+
+        public List<Project> GetListOfProjects(int customerId)
+        {
+
+            string sql;
+            SqlConnection cn;
+            SqlCommand cmd;
+            SqlDataReader dr;
+            List<Project> projects;
+            projects = new List<Project>();
+
+            sql = "SELECT * FROM Project WHERE CustomerId = " + customerId.ToString();
+            cn = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            cmd = new SqlCommand(sql, cn);
+            try
+            {
+                cn.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    List<Task> tasks = new List<Task>();
+                    Sql = "SELECT * FROM Task WHERE ProjectId= '" + dr["ProjectId"].Tostring() + "'";
+                    cn2 = new SqlConnection(Properties.Settings.Default.ConnectionString);
+                    cmd2 = new SqlCommand(sql, cn2);
+                    cn2.Open();
+                    dr2 = cmd2.ExecuteReader();
+                    while (dr2.Read())
+                    {
+                        Task t = new Task((Guid)dr2["TaskId"], dr2["Name"].ToString(), (Status)dr2["StatusId"]);
+                        tasks.Add(t);
+                    }
+                    dr2.Close();
+                    Project p = new Project(dr["Name"].ToString(), (DateTime)dr["ExpectedStartDate"], (DateTime)dr["ExpectedEndDate"], (Guid)dr["ProjectId"], tasks);
+                    projects.Add(p);
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Getting list", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
---
+++ PTSLibrary/PTSLibrary/PTSClientFacade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTSLibrary
+
+{
+    class PTSClientFacade : PTSSuperFacade
+    {
+        private DAO.ClientDAO dao;
+
+        public PTSClientFacade() : base(new DAO.ClientDAO())
+        {
+            dao = (DAO.ClientDAO)base.dao;
+        }
+
+        public TeamLeader Authenticate(string username, string password)
+        {
+            if (username == "" || password == "")
+            {
+                throw new Exception("Missing Data");
+            }
+            return dao.Authenticate(username, password);
+        }
+
+        public Project[] GetListOfProjects(int teamId)
+        {
+            return (dao.GetListOfProject(teamId)).ToArray();
+        }
+    }
+}
+
+
